fd92decdee1da4a98b7b602a8b49ceddf8d1a9b1
Maxxil <massil.kadi@ynov.com>
2017-02-06 11:41:57 +0100

    commit

---
+++ WebDevice/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2016 Microsoft.  All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebDevice
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
